50a097883039a1f23d198a7eb6a88d7682039684
Pawel Lewandowski <pawel.lewandowski@onewelcome.com>
2023-04-25 11:21:09 +0200

    Delete target file before moving

--- src/main/java/us/hebi/sass/PlatformUtil.java
+++ src/main/java/us/hebi/sass/PlatformUtil.java
@@ -40,113 +40,121 @@ import java.util.Locale;
  */
 public class PlatformUtil {
 
-    public static int BUFFER_SIZE = 1024;
-    public static String getDefaultArchiveExtension() {
-        return isWindows() ? "zip" : "tar.gz";
+  public static String getDefaultArchiveExtension() {
+    return isWindows() ? "zip" : "tar.gz";
+  }
+
+  public static String toScriptName(String name) {
+    return isWindows() ? name + ".bat" : name;
+  }
+
+  public static void downloadAndExtractArchive(URL url, Path destinationDirectory) throws IOException {
+    // Download
+    Path tmpArchive = destinationDirectory.resolve(createTemporaryArchiveName(url.toExternalForm()));
+    try (InputStream downloadFile = url.openStream()) {
+      Files.createDirectories(destinationDirectory);
+      Path tmpDownloadFile = Files.createTempFile(destinationDirectory, null, null);
+      FileUtils.copyInputStreamToFile(downloadFile, tmpDownloadFile.toFile());
+      if (tmpArchive.toFile().exists()) {
+        tmpArchive.toFile().delete();
+      }
+      Files.move(tmpDownloadFile, tmpArchive, StandardCopyOption.ATOMIC_MOVE);
     }
 
-    public static String toScriptName(String name) {
-        return isWindows() ? name + ".bat" : name;
+    // Extract
+    extractArchive(tmpArchive, destinationDirectory);
+
+    // Cleanup
+    Files.delete(tmpArchive);
+  }
+
+  private static String createTemporaryArchiveName(String url) {
+    if (url.endsWith(".zip")) {
+      return "archive.zip";
+    } else if (url.endsWith(".tar.gz")) {
+      return "archive.tar.gz";
+    } else {
+      throw new UnsupportedOperationException("Unsupported archive extension on: " + url);
+    }
+  }
+
+  public static void extractArchive(Path sourceArchive, Path destinationDirectory) {
+    // Create appropriate unarchiver
+    final AbstractUnArchiver unArchiver;
+    String path = sourceArchive.toString().toLowerCase(Locale.ENGLISH);
+    if (path.endsWith(".zip")) {
+      unArchiver = new ZipUnArchiver();
+    } else if (path.endsWith(".tar.gz")) {
+      TarGZipUnArchiver untar = new TarGZipUnArchiver();
+      untar.setCompression(UntarCompressionMethod.GZIP);
+      unArchiver = untar;
+    } else {
+      throw new UnsupportedOperationException("Unsupported archive extension on: " + path);
     }
 
-    public static void downloadAndExtractArchive(URL url, Path destinationDirectory) throws IOException {
-        // Download
-        Path tmpArchive = destinationDirectory.resolve(createTemporaryArchiveName(url.toExternalForm()));
-        try (InputStream downloadFile = url.openStream()) {
-            Files.createDirectories(destinationDirectory);
-            Path tmpDownloadFile = Files.createTempFile(destinationDirectory, null, null);
-            FileUtils.copyInputStreamToFile(downloadFile, tmpDownloadFile.toFile());
-            Files.move(tmpDownloadFile, tmpArchive, StandardCopyOption.ATOMIC_MOVE);
-        }
+    // Extract to specified directory
+    unArchiver.setSourceFile(sourceArchive.toFile());
+    unArchiver.setOverwrite(true);
+    unArchiver.setDestDirectory(destinationDirectory.toFile());
+    unArchiver.extract();
+  }
 
-        // Extract
-        extractArchive(tmpArchive, destinationDirectory);
+  public enum Architecture {
+    x86_32, x86_64, arm_32, arm_64
+  }
 
-        // Cleanup
-        Files.delete(tmpArchive);
+  public static Architecture getArch() {
+    // https://stackoverflow.com/a/36926327/3574093
+    switch (OS_ARCH) {
+      case "aarch64":
+        return Architecture.arm_64;
+      case "arm":
+        return Architecture.arm_32;
+      case "amd64":
+      case "ia64":
+      case "x86_64":
+        return Architecture.x86_64;
+      case "x86":
+      case "i386":
+      case "i486":
+      case "i586":
+      case "i686":
+        return Architecture.x86_32;
+      default:
+        throw new UnsupportedOperationException("Unsupported arch: " + System.getProperty("os.arch"));
     }
+  }
 
-    private static String createTemporaryArchiveName(String url) {
-        if (url.endsWith(".zip")) {
-            return "archive.zip";
-        } else if (url.endsWith(".tar.gz")) {
-            return "archive.tar.gz";
-        } else {
-            throw new UnsupportedOperationException("Unsupported archive extension on: " + url);
-        }
+  public enum OsFamily {
+    Windows, Linux, macOS;
+  }
+
+  public static OsFamily getOsFamily() {
+    if (isWindows()) {
+      return OsFamily.Windows;
     }
-
-    public static void extractArchive(Path sourceArchive, Path destinationDirectory) {
-        // Create appropriate unarchiver
-        final AbstractUnArchiver unArchiver;
-        String path = sourceArchive.toString().toLowerCase(Locale.ENGLISH);
-        if (path.endsWith(".zip")) {
-            unArchiver = new ZipUnArchiver();
-        } else if (path.endsWith(".tar.gz")) {
-            TarGZipUnArchiver untar = new TarGZipUnArchiver();
-            untar.setCompression(UntarCompressionMethod.GZIP);
-            unArchiver = untar;
-        } else {
-            throw new UnsupportedOperationException("Unsupported archive extension on: " + path);
-        }
-
-        // Extract to specified directory
-        unArchiver.setSourceFile(sourceArchive.toFile());
-        unArchiver.setOverwrite(true);
-        unArchiver.setDestDirectory(destinationDirectory.toFile());
-        unArchiver.extract();
+    if (isLinux()) {
+      return OsFamily.Linux;
     }
-
-    public enum Architecture {
-        x86_32, x86_64, arm_32, arm_64
+    if (isMac()) {
+      return OsFamily.macOS;
     }
+    throw new UnsupportedOperationException("Unsupported OS: " + System.getProperty("os.name"));
+  }
 
-    public static Architecture getArch() {
-        // https://stackoverflow.com/a/36926327/3574093
-        switch (OS_ARCH) {
-            case "aarch64":
-                return Architecture.arm_64;
-            case "arm":
-                return Architecture.arm_32;
-            case "amd64":
-            case "ia64":
-            case "x86_64":
-                return Architecture.x86_64;
-            case "x86":
-            case "i386":
-            case "i486":
-            case "i586":
-            case "i686":
-                return Architecture.x86_32;
-            default:
-                throw new UnsupportedOperationException("Unsupported arch: " + System.getProperty("os.arch"));
-        }
-    }
+  public static boolean isWindows() {
+    return OS_NAME.startsWith("win");
+  }
 
-    public enum OsFamily {
-        Windows, Linux, macOS;
-    }
+  public static boolean isMac() {
+    return OS_NAME.contains("mac");
+  }
 
-    public static OsFamily getOsFamily() {
-        if (isWindows()) return OsFamily.Windows;
-        if (isLinux()) return OsFamily.Linux;
-        if (isMac()) return OsFamily.macOS;
-        throw new UnsupportedOperationException("Unsupported OS: " + System.getProperty("os.name"));
-    }
+  public static boolean isLinux() {
+    return OS_NAME.startsWith("linux");
+  }
 
-    public static boolean isWindows() {
-        return OS_NAME.startsWith("win");
-    }
-
-    public static boolean isMac() {
-        return OS_NAME.contains("mac");
-    }
-
-    public static boolean isLinux() {
-        return OS_NAME.startsWith("linux");
-    }
-
-    public static final String OS_NAME = System.getProperty("os.name").toLowerCase(Locale.US);
-    public static final String OS_ARCH = System.getProperty("os.arch").toLowerCase(Locale.US);
+  public static final String OS_NAME = System.getProperty("os.name").toLowerCase(Locale.US);
+  public static final String OS_ARCH = System.getProperty("os.arch").toLowerCase(Locale.US);
 
 }
